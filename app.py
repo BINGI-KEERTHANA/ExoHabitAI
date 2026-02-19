a64e2e7919911c51503ca4bdbb8896855275105a
BINGI-KEERTHANA <keerthanabingi19@gmail.com>
2026-02-19 21:06:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from utils import load_model
 from flask import Flask, request, jsonify
+from flask_cors import CORS   # ✅ ADD THIS
 
 app = Flask(__name__)
+CORS(app)   # ✅ ADD THIS
 
 # LOAD MODEL ONCE AT STARTUP
 model = load_model()
