a0cf30109a5dd86950e5a1f996f8a345446fce05
BINGI-KEERTHANA <keerthanabingi19@gmail.com>
2026-02-19 19:21:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,13 +3,12 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-MODEL_PATH = os.path.join(
-    BASE_DIR,
-    "models",
-    "logistic_regression_final.pkl"
-)
+MODEL_PATH = os.path.join(BASE_DIR, "models", "logistic_regression_final.pkl")
 
 def load_model():
+    if not os.path.exists(MODEL_PATH):
+        raise FileNotFoundError(f"Model not found at {MODEL_PATH}")
     return joblib.load(MODEL_PATH)
+
 
 
